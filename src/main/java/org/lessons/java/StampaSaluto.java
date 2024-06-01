75c5adf4d847eb1d87745f1c54a48fd9028e1019
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-06-01 23:08:57 +0200

    added class StampaSaluto & main with  System.out.printf
    main with System.out.printf()

---
+++ src/main/java/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,10 @@
+package org.lessons.java; // package place x class
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("HELLO JAVA!");
+	}
+
+}
